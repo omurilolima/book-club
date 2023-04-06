fd5f51518c0e0a1ad3997c320d5c2f2901504802
Murilo Lima <msl@cesar.school>
2023-04-06 13:00:55 +0000

    Display comment form in post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -21,11 +22,12 @@ class PostDetail(View):
             liked = True
 
         return render(
-            request, 
+            request,
             'post_detail.html',
             {
                 'post': post,
                 'comments': comments,
-                'liked': liked
+                'liked': liked,
+                'comment_form': CommentForm()
             }
         )
--- bookclub/settings.py
+++ bookclub/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'django_summernote',
     'blog',
 ]
@@ -54,6 +55,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
