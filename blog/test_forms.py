0f07af33b21d6fdee841ac300db10898f4f5529f
Murilo Lima <msl@cesar.school>
2023-04-28 15:23:39 +0000

    Add automated tests for CommentForm

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from .forms import CommentForm, BookForm
 
 
-class TestDjango(TestCase):
+class TestBookForm(TestCase):
     """
     Testing fields of the BookForm
     used for creating a book
@@ -44,3 +44,19 @@ class TestDjango(TestCase):
             'data_started_reading',
             'date_finished_reading',
         ))
+
+
+class TestCommentForm(TestCase):
+    """
+    Testing fields of the CommentForm
+    used for leaving a comment in a blog post
+    """
+    def test_body_is_required(self):
+        form = CommentForm({'body': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('body', form.errors.keys())
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
+
+    def test_field_is_explicit_in_form_metaclass(self):
+        form = CommentForm()
+        self.assertEqual(form.Meta.fields, ('body',))
