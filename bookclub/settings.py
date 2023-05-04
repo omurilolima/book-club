4b69ac2ef8e30eb45e123522d0c5a9980567a078
Murilo Lima <msl@cesar.school>
2023-05-04 19:37:06 +0000

    Fix the responsiveness of the banner hero in the homepage

--- bookclub/settings.py
+++ bookclub/settings.py
@@ -104,22 +104,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bookclub.wsgi.application'
 
 
-# Database
+# Database for testing
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+# Database for production
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
